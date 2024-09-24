f357713fa0177ea1f77471a7277a406816bdedec
Diego <odiegodev@gmail.com>
2024-09-23 22:28:39 -0300

    test: add db testing

---
+++ fast_zero/tests/test_db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import select
+
+from fast_zero.models import User
+
+
+def test_create_user(session):
+    user = User(username='test', email='test@test.com', password='secret')
+    session.add(user)
+    session.commit()
+
+    result = session.scalar(select(User).where(User.email == 'test@test.com'))
+
+    assert result.id == 1
+    assert result.username == 'test'
+    assert result.password == 'secret'
